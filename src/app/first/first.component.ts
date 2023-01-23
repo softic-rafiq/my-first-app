f73a0391a66d84e761ce3a3f8cba976b2d49d84f
Md Rafiqul Islam <119554804+softic-rafiq@users.noreply.github.com>
2023-01-23 11:25:13 +0600

    Two way binding

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -17,6 +17,7 @@ export class FirstComponent {
   items: any[] = ['Banana', 'Apple', 'Mango'];
 
   isDisabled: boolean = false;
+  myName: string = '';
   bookName = 'Clean Code';
   src =
     'https://m.media-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg';
@@ -33,5 +34,17 @@ export class FirstComponent {
 
   sentDataToServices() {
     this.channel.getDataFromComponent(this.title);
+  }
+
+  enableButton() {
+    this.isDisabled = true;
+  }
+  disableButton() {
+    this.isDisabled = false;
+  }
+
+  handleInput(event: any) {
+    console.log(event.target.value);
+    this.myName = event.target.value;
   }
 }
